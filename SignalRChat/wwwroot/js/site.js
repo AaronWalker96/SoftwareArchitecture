ffcb12be5252cfc63338ea8eae9d04f3d237182d
Aaron Walker <aaron45587@gmail.com>
2018-11-10 17:52:02 +0000

    q	w

--- SignalRChat/wwwroot/js/site.js
+++ SignalRChat/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Clear the content of the message box
+// Clear the content of the message box 
 function clearMessageBox() {
     document.getElementById("messageInput").value = "";
 }
