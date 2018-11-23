fc3f8768449e81430293489cade9e0f4a9a3c873
Aaron Walker <aaron45587@gmail.com>
2018-11-23 14:09:41 +0000

    Added Identity

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
 namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
+        //[Authorize]
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
